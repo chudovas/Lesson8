522ca3785abb91ba0dd2128a432edc41c1ed863b
Гурьев Василий Александрович <Vaschudo@gmail.com>
2018-06-06 10:26:00 +0300

    Add unit test and fix code style.

--- Async/Async.fs
+++ Async/Async.fs
@@ -2,35 +2,34 @@
 open System.IO
 open System.Text.RegularExpressions
 
-let findAllUrlInTex text =
+let findAllUrlInText text =
     let urlRegex = Regex("<a href\s*=\s*\"(https?://[^\"]+)\"\s*>", RegexOptions.Compiled)
     [for m in urlRegex.Matches(text) -> m.Groups.[1].Value]
 
+let downloadPage (url : string) = 
+    async {
+        try
+            let request = WebRequest.Create(url)
+            use! response = request.AsyncGetResponse()
+            use stream = response.GetResponseStream()
+            use reader = new StreamReader(stream)
+            let text = reader.ReadToEnd() 
+            return (url, true, text)
+        with
+        | error -> return (url, false, error.Message)
+    }
 
 let printStatisticForUrl url =
     let urlRegex = Regex("<a href\s*=\s*\"(https?://[^\"]+)\"\s*>", RegexOptions.Compiled)
     
-    let DownloadPage (url : string) = 
-        async {
-            try
-                let request = WebRequest.Create(url)
-                use! response = request.AsyncGetResponse()
-                use stream = response.GetResponseStream()
-                use reader = new StreamReader(stream)
-                let text = reader.ReadToEnd() 
-                return (url, true, text)
-            with
-            | error -> return (url, false, error.Message)
-        }
-    
-    let mainPageText = url |> DownloadPage |> Async.RunSynchronously
+    let mainPageText = url |> downloadPage |> Async.RunSynchronously
     
     match mainPageText with
     | (_, false, errText) -> 
         printfn "Error with main url! %s" errText
         false
     | (_, true, text) ->
-        let allSitesAndTexts = [for u in findAllUrlInTex text -> DownloadPage(u)] |> Async.Parallel |> Async.RunSynchronously
+        let allSitesAndTexts = [for u in findAllUrlInText text -> downloadPage(u)] |> Async.Parallel |> Async.RunSynchronously
         for el in allSitesAndTexts do
             match el with
             | (url, false, errText) -> printfn "Error with %s url! %s" url errText
--- Test/Test.fs
+++ Test/Test.fs
@@ -6,19 +6,25 @@ open Async
 
 [<Test>]
 let ``test of correct regex 1``() =
-    findAllUrlInTex "djnfvksjnfvjanvjfshbdvofohbvofhbvohd" |> should equal []
+    findAllUrlInText "djnfvksjnfvjanvjfshbdvofohbvofhbvohd" |> should equal []
 
 [<Test>]
 let ``test of correct regex 2``() =
-    findAllUrlInTex "<a href=\"http://1\"><a href=\"http://2\"><a href=\"http://3\">" |> should equal ["http://1"; "http://2"; "http://3"]
+    findAllUrlInText "<a href=\"http://1\"><a href=\"http://2\"><a href=\"http://3\">" |> should equal ["http://1"; "http://2"; "http://3"]
 
 [<Test>]
 let ``test of fail on uncorrect url``() =
     printStatisticForUrl "jfnkjvndfkv" |> should equal false
+
+[<Test>]
+let ``test of loading page``() =
+    let (_, _, text) = ("https://yandex.ru" |> downloadPage |> Async.RunSynchronously)
+    (text.Length > 0) |> should equal true
 
 [<EntryPoint>]
 let main argv =
     ``test of correct regex 1``()
     ``test of correct regex 2``()
     ``test of fail on uncorrect url``()
+    ``test of loading page``()
     0
